96aa772380f123823c521b7df01f83d19c6c60f8
Jerónimo Salinas Guerra <salinasjeronimo579@gmail.com>
2026-09-01 17:55:08 -0500

    chore: creacion de la conexion a la base de datos

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import conn from "./src/config/database.js";
+
+const app = express()
+
+const PORT = 3000
+const SERVER = "http://localhost:"
+const URL = SERVER + PORT
+
+app.listen(PORT, () => {
+    console.log("El servidor esta nitido en: " + URL)
+})
+
+conn.authenticate()
+    .then(() => {
+        return conn.sync()
+    })
+    .then(() => console.log("Conexión nitida"))
+    .catch((error) => console.log(error))
---
+++ src/config/creedentials.js
@@ -0,0 +1,12 @@
+const DB_NAME = "biblioteca";
+const DB_USER = "root";
+const DB_PASSWORD = "dawnsbalad1234*"
+const OBJ_CONN = {
+    port: 3306,
+    dialect: "mysql",
+    host: "localhost"
+}
+
+export {
+    DB_NAME, DB_USER, DB_PASSWORD, OBJ_CONN
+}
